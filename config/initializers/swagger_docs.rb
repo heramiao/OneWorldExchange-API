b8b9a64c8bc3ea5abdf8187285d08b72b548aa81
Hera Miao <heramiao@gmail.com>
2018-11-19 18:47:48 -0500

    changed swagger docs base

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -16,7 +16,7 @@ class Swagger::Docs::Config
       # the output location where your .json files are written to
       :api_file_path => "public/apidocs",
       # the URL base path to your API (make sure to change this if you are not using localhost:3000)
-      :base_path => "http://localhost:3000",
+      :base_path => "https://oneworldexchange.herokuapp.com",
       # if you want to delete all .json files at each generation
       :clean_directory => false,
       # add custom attributes to api-docs
