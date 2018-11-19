b05969450e90c0b2e29383e3f9a5dbc2f8ccc626
Hera Miao <heramiao@gmail.com>
2018-11-19 18:27:48 -0500

    changed migrations

--- db/migrate/20181023024237_create_group_members.rb
+++ db/migrate/20181023024237_create_group_members.rb
@@ -1,7 +1,8 @@
 class CreateGroupMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :group_members do |t|
-
+      t.references :travel_group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20181029171154_add_foreign_keys.rb
+++ db/migrate/20181029171154_add_foreign_keys.rb
@@ -1,9 +1,6 @@
 class AddForeignKeys < ActiveRecord::Migration[5.2]
   def change
-    add_reference :group_members, :users, foreign_key: true
-    add_reference :group_members, :travel_groups, foreign_key: true
-
-    add_reference :trips, :travel_groups, foreign_key: true
-    add_reference :trips, :transactions, foreign_key: true
+    add_reference :trips, :travel_group, foreign_key: true
+    add_reference :trips, :transaction, foreign_key: true
   end
 end
